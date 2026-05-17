714152cb54d6f98d302c7179f7dae56534ba5a21
Pow Ehrmantrut <pedrohfdias.15122007@gmail.com>
2026-05-17 00:22:09 -0300

    1

--- bot.py
+++ bot.py
@@ -1,5 +1,5 @@
 import discord
-from discord import app_commands
+from discord import app_commands, ui
 from discord.ext import commands
 import os  # <-- CERTIFIQUE-SE DE QUE ESTA LINHA ESTÁ AQUI
 import re
